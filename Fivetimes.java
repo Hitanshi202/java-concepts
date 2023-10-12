69164e7d0e92d42a389ab7471ee33edf77e7212b
Hitanshi202 <147725309+Hitanshi202@users.noreply.github.com>
2023-10-12 18:41:44 +0530

    Add files via upload

---
+++ Fivetimes.java
@@ -0,0 +1,21 @@
+public class Fivetimes
+{
+    public static void main(String[] args)
+    {
+        for (int i=1;i<=5;i++)
+        {
+            //no. of co/lumns
+            for(int j=1;j<=i;j++)
+            //i=1 1,i=2,22
+                //j<=3 it means 3 times 111
+
+            {
+                System.out.print(j);
+                //print remove ln same line
+//                j 12345 12345....
+            }
+            System.out.println("");
+        }
+
+    }
+}
