9360ceb88a053c43d301b390bcee6e040a63fe33
Hitanshi202 <147725309+Hitanshi202@users.noreply.github.com>
2023-10-25 22:51:55 +0530

    Add files via upload

---
+++ addsum.java
@@ -0,0 +1,20 @@
+public class addsum
+{
+}
+class ABC
+{
+  static  int addnum(int x,int y)
+    {
+        return x+y;
+    }
+
+}
+class XYZ
+{
+    public static void main(String[] args)
+    {
+        System.out.println(ABC.addnum(5,4));
+
+    }
+}
+
