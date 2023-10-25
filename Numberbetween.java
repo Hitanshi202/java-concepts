11bf645d8053ad02e82b12fdcb395ff46a958691
Hitanshi202 <147725309+Hitanshi202@users.noreply.github.com>
2023-10-25 23:03:02 +0530

    Add files via upload

---
+++ Numberbetween.java
@@ -0,0 +1,15 @@
+public class Numberbetween
+{
+    public static void main(String[] args)
+    {
+        int num =9;
+        if((num>=10) &&(num<=20))
+        {
+            System.out.println("Number lies between 10 to 20 ");
+        }
+        else
+        {
+            System.out.println("Number not lies between 10 to 20");
+        }
+    }
+}
