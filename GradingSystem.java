7e88c0e055a4c1dba7d1ae8af9347fdeaf081553
Hitanshi202 <147725309+Hitanshi202@users.noreply.github.com>
2023-10-25 22:55:48 +0530

    Add files via upload

---
+++ GradingSystem.java
@@ -0,0 +1,21 @@
+public class GradingSystem
+{
+    public static void main(String[] args)
+    {
+     int marks =56;
+     if (marks>=80 && marks<=100)
+     {
+         System.out.println("you have A grade");
+     } else if (marks>=60 && marks<80)
+     {
+         System.out.println("you have B grade");
+     } else if (marks>=45 && marks<60)
+     {
+         System.out.println("you have C grade");
+     } else if (marks>=33 && marks<45)
+     {
+         System.out.println("you have D grade");
+
+     }
+    }
+}
