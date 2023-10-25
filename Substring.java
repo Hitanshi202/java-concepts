87d39330e3b82471f394f69a29bd0216b3267053
Hitanshi202 <147725309+Hitanshi202@users.noreply.github.com>
2023-10-25 22:59:51 +0530

    Add files via upload

---
+++ Substring.java
@@ -0,0 +1,17 @@
+public class Substring
+{
+    public static void main(String[] args)
+    {
+        String string ="hello hit";
+     String substring = "hello";
+     if(string.contains(substring))
+     {
+            System.out.println("this string contains substring hello");
+     }
+     else
+     {
+         System.out.println("this string not contains substring hello");
+     }
+
+    }
+}
