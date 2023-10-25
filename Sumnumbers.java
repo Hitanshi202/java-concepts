ae86fce6c2bf0c988fef26bf41847808acf93fe8
Hitanshi202 <147725309+Hitanshi202@users.noreply.github.com>
2023-10-25 22:54:15 +0530

    Add files via upload

---
+++ Sumnumbers.java
@@ -0,0 +1,13 @@
+public class Sumnumbers
+{
+    public static void main(String[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                sum += i;
+            }
+            System.out.println("The sum of all numbers between 1 to 10 is: " + sum);
+        }
+}
+
