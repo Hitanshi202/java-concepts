2b8c6f7ce7fcfed1e39775743bd0e1efd091165b
Hitanshi202 <147725309+Hitanshi202@users.noreply.github.com>
2023-10-25 22:58:20 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,16 @@
+public class Typecasting
+{
+    public static void main(String[] args)
+    {
+        int num=2;
+        int num2=3;
+        int sum=(num+num2);
+        System.out.println((float)sum);
+        short s1=12345;
+        System.out.println((int)s1);
+        float num3=3.14f;
+        System.out.println((int)num3);
+        System.out.println(num+" , " +num2);
+
+    }
+}
