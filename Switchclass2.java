bf8282c8fecd93b657d1a81d8e38f831cf72ab08
Hitanshi202 <147725309+Hitanshi202@users.noreply.github.com>
2023-10-25 22:58:43 +0530

    Add files via upload

---
+++ Switchclass2.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Switchclass2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the month:");
+        String month= sc.next();
+         switch(month)
+        {
+            case"january":
+                System.out.println("31");
+                break;
+            case"february":
+                System.out.println("28");
+                break;
+            case"march":
+                System.out.println("31");
+                break;
+            case"april":
+                System.out.println("30");
+                break;
+            case"may":
+                System.out.println("31");
+                break;
+            case"june":
+                System.out.println("30");
+                break;
+            case"july":
+                System.out.println("31");
+                break;
+            case"august":
+                System.out.println("31");
+                break;
+            case"september":
+                System.out.println("30");
+                break;
+            case"october":
+                System.out.println("31");
+                break;
+            case"november":
+                System.out.println("30");
+                break;
+            case"december":
+                System.out.println("31");
+                break;
+            default:
+                System.out.println("invalid month");
+        }
+    }
+}
+
+
+
+
+
